a88839075f06c4ab2115dec808c98bbbb1610696
Marco De Jesus <marco_dejesus@live.com>
2021-03-26 11:37:59 -0600

    Haciendo uso de la clase CreadorDeArchivosCsv para crear un archivo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String []args){
+    public static void main(String []args) throws IOException {
+
+        CreadorDeArchivosCSV creadorCsv = new CreadorDeArchivosCSV();
+        var archivoCreado = creadorCsv.crearArchivoCsv("archivo");
 
     }
 }
