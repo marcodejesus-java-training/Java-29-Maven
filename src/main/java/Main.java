3c19bb231904ecda0c6f495c63fe8a347f3250a1
Marco De Jesus <marco_dejesus@live.com>
2021-03-26 11:50:24 -0600

    Imprimiendo el contenido del Archivo csv creado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,29 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 
 public class Main {
     public static void main(String []args) throws IOException {
-
+        String nombreDelArchivo = "archivo";
         CreadorDeArchivosCSV creadorCsv = new CreadorDeArchivosCSV();
-        var archivoCreado = creadorCsv.crearArchivoCsv("archivo");
 
+        var archivoCreado = creadorCsv.crearArchivoCsv(nombreDelArchivo);
+
+        if(archivoCreado){
+            Reader lectorDeArchivo = new FileReader(nombreDelArchivo+".csv");
+
+            Iterable<CSVRecord> registros = CSVFormat.DEFAULT
+                    .withHeader(creadorCsv.Encabezados)
+                    .withFirstRecordAsHeader()
+                    .parse(lectorDeArchivo);
+
+            for (var registro : registros) {
+                System.out.print(registro.get("Nombre") + " " + registro.get("Apellido Paterno"));
+                System.out.println();
+            }
+        }
     }
 }
